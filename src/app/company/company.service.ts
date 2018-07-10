b0b0929826ad47c747acf81a8af8dd1dcc8dd790
João Paulo Lins <jplo@cin.ufpe.br>
2018-07-09 21:07:15 -0300

    ajuste

--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Params } from '@angular/router';
-import * as Amqp from 'amqp-ts';
+import * as amqp from 'amqplib';
+// import * as Amqp from 'amqp-ts';
 
 @Injectable({
   providedIn: 'root'
@@ -23,16 +24,28 @@ export class CompanyService {
 
   tracker(params?: Params) {
 
+    amqp.connect('amqp://172.17.0.2', function(err, conn) {
+      conn.createChannel(function(err, ch) {
+        const q = 'hello';
+
+        ch.assertQueue(q, {durable: false});
+        console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', q);
+        ch.consume(q, function(msg) {
+          console.log(' [x] Received %s', msg.content.toString());
+        }, {noAck: true});
+      });
+    });
+
     // TODO: Implementar o socket para receber os Trackers do socket do back-end
     // this.socket = io('http://localhost:8080');
     // return this.httpClient.get(`http://localhost:8080/companies/${params.name}/bus/${params.registration}`);
 
 
-    const connection = new Amqp.Connection('amqp://172.17.0.2');
-    const queue = connection.declareQueue('front');
-    queue.activateConsumer((message) => {
-        console.log('Message received: ' + message.getContent());
-        // return Observable.of(message.getContent());
-    });
+    // const connection = new Amqp.Connection('amqp://172.17.0.2');
+    // const queue = connection.declareQueue('front');
+    // queue.activateConsumer((message) => {
+    //     console.log('Message received: ' + message.getContent());
+    //     // return Observable.of(message.getContent());
+    // });
   }
 }
