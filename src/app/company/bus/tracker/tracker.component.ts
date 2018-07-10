50bea7ebe6855c613dddf9b56ca0f8afa77ed815
João Paulo Lins <jplo@cin.ufpe.br>
2018-07-09 22:01:11 -0300

    Revert "Ajuste tracker"
    This reverts commit 02a5a50247a469fdcdbeae4eea42466e409a6c23.

--- src/app/company/bus/tracker/tracker.component.ts
+++ src/app/company/bus/tracker/tracker.component.ts
@@ -11,11 +11,27 @@ export class TrackerComponent implements OnInit {
 
   private companyService: CompanyService;
 
-  // public tracket: Tracker;
   public tracker: Tracker =
     {latitude: 51.678418,
     longitude: 7.809007,
     time: '12h30'};
+  // public trackers: Tracker[] = [
+  //   {latitude: 123242,
+  //   longitude: 801403,
+  //   time: '12h30'},
+  //   {latitude: 123243,
+  //   longitude: 801403,
+  //   time: '12h31'},
+  //   {latitude: 123244,
+  //   longitude: 801403,
+  //   time: '12h32'},
+  //   {latitude: 123245,
+  //   longitude: 801403,
+  //   time: '12h35'},
+  //   {latitude: 123246,
+  //   longitude: 801403,
+  //   time: '12h36'}
+  // ];
 
   constructor() { }
 
